4e607b18e8332e28e58642eeb3086557a6c7afd5
Don Park <don@donpark.org>
2008-09-30 15:21:58 -0700

    create a default identity if one does not exist.

--- src/com/icecondor/nest/Start.java
+++ src/com/icecondor/nest/Start.java
@@ -1,10 +1,13 @@
 package com.icecondor.nest;
 
+import java.util.UUID;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -25,6 +28,20 @@ public class Start extends Activity implements ServiceConnection {
     private void restorePreferences() {
 		Log.i(appTag, "restorePreferences()");
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		Editor editor = settings.edit();
+
+        // Set the unique ID
+		String uuid;
+		if(settings.contains("uuid")) {
+			uuid = settings.getString("uuid", null);
+			Log.i(appTag, "retrieved UUID of "+uuid);
+		} else {
+			uuid = UUID.randomUUID().toString();
+			editor.putString("uuid", uuid);
+			Log.i(appTag, "no UUID in preferences. generated "+uuid);
+		}
+		
+		// Start the Pigeon
         boolean startPigeon = settings.getBoolean("startPigeon", true);
         if (startPigeon) {
             try {
@@ -34,6 +51,7 @@ public class Start extends Activity implements ServiceConnection {
 				e.printStackTrace();
 			}
         }
+        
 	}
 
 	private void startPigeon() {
