3996007b73e7335b6d7c0f26e4159ab08a234995
Don Park <don@donpark.org>
2008-10-14 21:08:02 -0700

    start a second thread while in the Radar activity to read from icecondor

--- src/com/icecondor/nest/Radar.java
+++ src/com/icecondor/nest/Radar.java
@@ -1,11 +1,22 @@
 package com.icecondor.nest;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -19,23 +30,28 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
-public class Radar extends MapActivity implements ServiceConnection {
+public class Radar extends MapActivity implements ServiceConnection,
+												  Constants {
 	static final String appTag = "Radar";
 	MapController controller;
 	PigeonService pigeon;
 	private static final long UPDATE_INTERVAL = 5000;
+	private Timer pigeon_poll_timer = new Timer();
+	private Timer service_read_timer = new Timer();
+	SharedPreferences settings;
+	String URL = "http://icecondor.com/locations"; // use preference
 	
     public void onCreate(Bundle savedInstanceState) {
     	Log.i(appTag, "onCreate");
         super.onCreate(savedInstanceState);
+        settings = getSharedPreferences(PREFS_NAME, 0);
         setContentView(R.layout.radar);
         Intent pigeon_service = new Intent(this, Pigeon.class);
         bindService(pigeon_service, this, 0); // 0 = do not auto-start
         MapView mapView = (MapView) findViewById(R.id.radar_mapview);
         controller = mapView.getController();
         controller.setZoom(9);
-    	Timer timer = new Timer();
-		timer.scheduleAtFixedRate(
+		pigeon_poll_timer.scheduleAtFixedRate(
 				new TimerTask() {
 					public void run() {
 						try {
@@ -49,7 +65,29 @@ public class Radar extends MapActivity implements ServiceConnection {
 						}
 					}
 				}, 0, UPDATE_INTERVAL);
+		service_read_timer.scheduleAtFixedRate(
+				new TimerTask() {
+					public void run() {
+						getNearbys();
+					}
+				}, 0, UPDATE_INTERVAL);
     }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	Log.i(appTag, "onResume yeah");
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+		unbindService(this);
+    	pigeon_poll_timer.cancel();
+    	service_read_timer.cancel();
+    	Log.i(appTag, "onPause yeah");
+    }
+
     
 	@Override
 	protected boolean isRouteDisplayed() {
@@ -81,8 +119,26 @@ public class Radar extends MapActivity implements ServiceConnection {
 
 	public void onServiceDisconnected(ComponentName className) {
 		Log.i(appTag, "onServiceDisconnected "+className);
-		
 	}
 
+	public void getNearbys() {
+		try {
+			Log.i(appTag, "send read request");
+			HttpClient client = new DefaultHttpClient();
+			URL = URL + "?id=wtf";
+			HttpGet get = new HttpGet(URL);
 
+			get.addHeader("X_REQUESTED_WITH", "XMLHttpRequest");
+			HttpResponse response;
+			response = client.execute(get);
+			Log.i(appTag, "http response: "+response.getStatusLine());
+		} catch (ClientProtocolException e) {
+			Log.i(appTag, "client protocol exception "+e);
+		} catch (HttpHostConnectException e) {
+			Log.i(appTag, "connection failed "+e);
+		} catch (IOException e) {
+			Log.i(appTag, "IO exception "+e);
+			e.printStackTrace();
+		}
+	}
 }
