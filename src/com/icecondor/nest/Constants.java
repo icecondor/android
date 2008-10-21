c5f62be80b72f49c35c3e550be9d5aa409e05b53
Don Park <don@donpark.org>
2008-10-20 21:04:46 -0700

    slow down the read and write values to 15 seconds

--- src/com/icecondor/nest/Constants.java
+++ src/com/icecondor/nest/Constants.java
@@ -3,7 +3,7 @@ package com.icecondor.nest;
 public interface Constants {
 	public static final String PREFS_NAME = "IceNestPrefs";
 	public static final String ICECONDOR_WRITE_URL = "http://icecondor.com/locations.json"; // use preference
-	public static final long PIGEON_LOCATION_POST_INTERVAL = 5000;
+	public static final long PIGEON_LOCATION_POST_INTERVAL = 15000;
 	public static final String ICECONDOR_READ_URL = "http://icecondor.com/locations"; // use preference
-	public static final long ICECONDOR_READ_INTERVAL = 5000;
+	public static final long ICECONDOR_READ_INTERVAL = 15000;
 }
