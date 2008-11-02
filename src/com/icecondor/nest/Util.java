6f6c5aab1ec334ab33944bc691f61e3204d3b9df
Don Park <donp@sparky.(none)>
2008-11-01 23:20:26 -0700

    report GMT to the server

--- src/com/icecondor/nest/Util.java
+++ src/com/icecondor/nest/Util.java
@@ -1,15 +1,16 @@
 package com.icecondor.nest;
 
-import java.sql.Date;
+import java.util.Date;
+import java.util.TimeZone;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class Util {
 	public static String DateTimeIso8601(long offset) {
-		DateFormat date_pattern =  new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss");
-		String date = date_pattern.format(new Date(offset));
+		DateFormat datePattern =  new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss");
+		datePattern.setTimeZone(TimeZone.getTimeZone("GMT"));
+		String date = datePattern.format(new Date(offset));
 	    // remap the timezone from 0000 to 00:00 (starts at char 22)
         return date;//.substring (0, 22) + ":" + date.substring (22);
-		//return "dateholder";
 	}
 }
