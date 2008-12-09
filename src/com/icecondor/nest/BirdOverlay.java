09fd63f37a97ead5ccd8b6281cacf9e2f8650461
Don Park <don@donpark.org>
2008-12-08 16:54:55 -0800

    display a blue dot where the last recorded fix is. add meters of accuracy to the radar status line

--- src/com/icecondor/nest/BirdOverlay.java
+++ src/com/icecondor/nest/BirdOverlay.java
@@ -1,24 +1,39 @@
 package com.icecondor.nest;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.location.Location;
+import android.util.Log;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 public class BirdOverlay extends Overlay {
+	String appTag = "BirdOverlay";
 	Location last_fix;
+	Point me = new Point();
+	Paint me_paint = new Paint();
+	java.text.DecimalFormat accuracy_format;
+	java.text.DecimalFormat latlong_format;
 	
 	public Location getLast_fix() {
 		return last_fix;
 	}
 	
 	public void setLast_fix(Location last_fix) {
+		Log.i("draw", "new last fix"+last_fix);
 		this.last_fix = last_fix;
 	}
 	
 	BirdOverlay() {
+		Log.i(appTag, "BirdOverlay constructed");
+		me_paint.setColor(Color.BLUE);
+		latlong_format = new java.text.DecimalFormat("###.######");
+		accuracy_format = new java.text.DecimalFormat("####");
+
 	}
 
 	@Override
@@ -34,9 +49,15 @@ public class BirdOverlay extends Overlay {
 		if (last_fix == null) {
 			msg = "waiting for fix";
 		} else {
-			java.text.DecimalFormat df = new java.text.DecimalFormat("###.######");
-			msg = df.format(last_fix.getLatitude()) + " " + df.format(last_fix.getLongitude()) +
-			      "    " + Util.timeAgoInWords(last_fix.getTime());
+			GeoPoint geo_me = new GeoPoint(
+                    (int)(last_fix.getLatitude()*1000000), 
+                    (int)(last_fix.getLongitude()*1000000));
+			mapView.getProjection().toPixels(geo_me, me);
+			canvas.drawCircle(me.x, me.y, 5, me_paint);
+			
+			msg = latlong_format.format(last_fix.getLatitude()) + " " + latlong_format.format(last_fix.getLongitude()) +
+			      "    " + Util.timeAgoInWords(last_fix.getTime()) + "  " + accuracy_format.format(last_fix.getAccuracy())
+			      + "m. acc.";
 		}
 		canvas.drawText(msg, 3, h - 53, mPaint);
 	}
