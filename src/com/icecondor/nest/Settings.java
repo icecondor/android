1d1f64d32a2e623a43799f1605d5facef6c6afbe
Don Park <don@donpark.org>
2008-12-08 17:03:56 -0800

    remove a bunch of old settings code thats not used any more

--- src/com/icecondor/nest/Settings.java
+++ src/com/icecondor/nest/Settings.java
@@ -10,7 +10,6 @@ import android.os.RemoteException;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,7 +21,6 @@ import android.widget.TabHost;
 import android.widget.TextView;
 
 public class Settings extends PreferenceActivity implements ServiceConnection,    
-															OnClickListener,
                                                             Constants {
 	TabHost myTabHost;
 	static final String appTag = "Settings";
@@ -62,30 +60,6 @@ public class Settings extends PreferenceActivity implements ServiceConnection,
     	Log.i(appTag, "onPause yeah");
     }
 
-	public void onTabChanged(String tabId) {
-		Log.i(appTag, "changed to tab "+tabId);
-		if (tabId == "TAB2") {
-			updateSettingTabWidgets();
-		}
-	}
-
-	private void updateSettingTabWidgets() {
-		Log.i(appTag, "updateSettingTabWidgets");
-		boolean checked;
-		try {
-			checked = isPigeonOn();
-		} catch (RemoteException e) {
-			checked = false;
-			Log.i(appTag, "cannot reach pigeon service to display in settings tab");
-		}
-		((RadioButton) findViewById(R.id.ibtn_settings_pigeon_on)).setChecked(checked);
-	}
-
-	private boolean isPigeonOn() throws RemoteException {
-		boolean result = pigeon.isTransmitting();
-        return result;
-	}
-
 	public void onServiceConnected(ComponentName className, IBinder service) {
 		Log.i(appTag, "onServiceConnected "+service);
 		pigeon = PigeonService.Stub.asInterface(service);
@@ -96,36 +70,12 @@ public class Settings extends PreferenceActivity implements ServiceConnection,
 		
 	}
 
-	public void onClick(View arg0) {
-		if (arg0.getId() == R.id.ibtn_settings_pigeon_on) {
-			try {
-				if(isPigeonOn() == true) {
-					pigeon.stopTransmitting();
-				} else {
-					pigeon.startTransmitting();
-				}
-				((RadioButton) findViewById(R.id.ibtn_settings_pigeon_on)).setChecked(isPigeonOn());
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-		} else if (arg0.getId() == R.id.settings_uuid) {
-			settings_layout.removeView(arg0);
-			if(arg0.getClass() == TextView.class) {
-				EditText edit_uuid = new EditText(this);
-				edit_uuid.setId(R.id.settings_uuid_edit);
-				edit_uuid.setText(settings.getString(SETTING_OPENID, "n/a"));
-				settings_layout.addView(edit_uuid, 2);
-			}
-		}
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		Log.i(appTag, "onCreateOptionsMenu");
 		boolean result = super.onCreateOptionsMenu(menu);
 		menu.add(0, Menu.FIRST, 0, ""+ICECONDOR_VERSION);
 		menu.add(0, R.string.menu_radar, 0, R.string.menu_radar).setIcon(android.R.drawable.ic_menu_compass);
-		menu.add(0, R.string.menu_settings, 0, R.string.menu_settings).setIcon(android.R.drawable.ic_menu_preferences);
 		return result;
 	}
 	
@@ -138,25 +88,7 @@ public class Settings extends PreferenceActivity implements ServiceConnection,
 		return false;
 	}
 
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		if(v.getId() == R.id.settings_uuid_edit) {
-			Log.i(appTag, "keycode: "+keyCode);
-			if(keyCode == 66) {
-				add_uuid_display(v);
-			}
-		}
-		return false;
-	}
 
-	private void add_uuid_display(View v) {
-		settings.edit().putString(SETTING_OPENID,((EditText)v).getText().toString()).commit();
-		settings_layout.removeView(v);
-		TextView uuid = new TextView(this);
-		uuid.setId(R.id.settings_uuid);
-		uuid.setOnClickListener(this);
-		uuid.setText(settings.getString(SETTING_OPENID, "n/a"));
-		settings_layout.addView(uuid, 2);
-	}
 
 	public void onFocusChange(View arg0, boolean arg1) {
 		// TODO Auto-generated method stub
