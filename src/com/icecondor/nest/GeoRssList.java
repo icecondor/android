606e3a472130def40431267aae9d052d1b140457
Don Park <don@donpark.org>
2008-12-11 13:13:31 -0800

    use the pre-made list stylings

--- src/com/icecondor/nest/GeoRssList.java
+++ src/com/icecondor/nest/GeoRssList.java
@@ -12,16 +12,16 @@ public class GeoRssList extends ListActivity {
         
 		GeoRssSqlite rssdb = new GeoRssSqlite(this, "georss", null, 1);
 		SQLiteDatabase db = rssdb.getWritableDatabase();
+		//db.execSQL("insert into urls values (null, 'service', 'https://service.com'");
         ListAdapter adapter = new SimpleCursorAdapter(
                 this, // Context
                 android.R.layout.two_line_list_item,  // Specify the row template to use (here, two columns bound to the two retrieved cursor rows)
                 db.query("urls",null, null, null, null, null, null),  // Pass in the cursor to bind to.
                 new String[] {GeoRssSqlite.NAME, GeoRssSqlite.URL}, // Array of cursor columns to bind to.
-                new int[] {R.id.text1, R.id.text2});      // Parallel array of which template objects to bind to those columns.
+                new int[] {android.R.id.text1, android.R.id.text2});      // Parallel array of which template objects to bind to those columns.
 
         // Bind to our new adapter.
         setListAdapter(adapter);
-
 
 	}
 }
