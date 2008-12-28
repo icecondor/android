fc29177277e8e274eb23496dba3326fc7f7cf127
Don Park <don@donpark.org>
2008-12-28 13:39:50 -0800

    add meat to the GeoRssDetail screen

--- src/com/icecondor/nest/GeoRssDetail.java
+++ src/com/icecondor/nest/GeoRssDetail.java
@@ -2,23 +2,56 @@ package com.icecondor.nest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class GeoRssDetail extends Activity {
 	private final String appTag = "GeoRssDetail";
+	GeoRssSqlite rssdb;
+	SQLiteDatabase geoRssDb;
 	private int row_id;
 	
     @Override
     protected void onCreate(Bundle icicle){
         super.onCreate(icicle);
         setContentView(R.layout.georssdetail);
+        
+		rssdb = new GeoRssSqlite(this, "georss", null, 1);
         row_id = getIntent().getExtras().getInt(GeoRssSqlite.ID);
         Log.i(appTag, "GeoRssDetail created with row id "+row_id);
-        
+    }
+
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	// fill in fields on the screen
+		geoRssDb = rssdb.getReadableDatabase();
+		Cursor service = geoRssDb.query(GeoRssSqlite.SERVICES_TABLE,null, "_id = ?",
+				                     new String[] {""+row_id}, null, null, null);
+		service.moveToFirst();
+		String name = service.getString(service.getColumnIndex(GeoRssSqlite.NAME));
+		String url = service.getString(service.getColumnIndex(GeoRssSqlite.URL));
+		TextView msgTextView = (TextView)findViewById(R.id.georssdetail_header);
+		msgTextView.setText(name);
+		TextView urlTextView = (TextView)findViewById(R.id.georssdetail_url);
+		urlTextView.setText(url);
+		
+		Cursor last_update = geoRssDb.query(GeoRssSqlite.SHOUTS_TABLE,null, "service_id = ?",
+                new String[] {""+row_id}, null, null, "date desc", "1");
+		String date;
+		if (last_update.getCount() > 0) {
+			last_update.moveToFirst();
+			date = last_update.getString(last_update.getColumnIndex("date"));
+		} else {
+			date = "no updates";
+		}
+		TextView dateTextView = (TextView)findViewById(R.id.georssdetail_lastupdate);
+		dateTextView.setText(date);
     }
     
 	public boolean onCreateOptionsMenu(Menu menu) {
