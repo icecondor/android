a8d95c0a2d5d5ef267f8f61995d22035640b6115
Don Park <don@donpark.org>
2009-12-17 20:51:17 -0800

    the ActivityLog

---
+++ src/com/icecondor/nest/ActivityLog.java
@@ -0,0 +1,59 @@
+package com.icecondor.nest;
+
+import com.icecondor.nest.db.GeoRss;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
+
+public class ActivityLog extends ListActivity {
+	GeoRss rssdb;
+	LogObserver logob;
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitylog);
+
+        rssdb = new GeoRss(this);
+		rssdb.open();		
+
+		Cursor logs = rssdb.findActivityLogs();
+        ListAdapter adapter=new SimpleCursorAdapter(this,
+                R.layout.activitylog_row, logs,
+                new String[] {"date", "description"},
+                new int[] {R.id.date, R.id.description});
+        logob = new LogObserver();
+        setListAdapter(adapter);
+	}
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean result = super.onCreateOptionsMenu(menu);
+		menu.add(Menu.NONE, 1, Menu.NONE, "Clear").setIcon(android.R.drawable.ic_delete);
+		return result;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 1:
+			rssdb.clearLog();
+			break;
+		}
+		return false;
+	}
+
+	class LogObserver extends DataSetObserver {
+		public void onChanged() {
+		}
+		public void onInvalidated() {
+			
+		}
+	}
+}
+
+
