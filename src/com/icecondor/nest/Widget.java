f9d82a57c21c1ecf6f9558a01a7d4410e20a4136
Don Park <don@donpark.org>
2011-10-27 15:15:18 -0700

    minor bird widget

--- src/com/icecondor/nest/Widget.java
+++ src/com/icecondor/nest/Widget.java
@@ -1,7 +1,17 @@
 package com.icecondor.nest;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
 
 public class Widget extends AppWidgetProvider {
-
+	static final String appTag = "Widget";
+	
+	@Override
+	public void onUpdate(Context ctxt,
+	AppWidgetManager mgr,
+	int[] appWidgetIds) {
+		Log.i(appTag, "onUpdate");
+	}
 }
