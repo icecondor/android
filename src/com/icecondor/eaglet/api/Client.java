062f4be11360dea91e1b89c44ca91f592e820be9
Don Park <donp@donp.org>
2014-05-17 10:39:01 -0700

    put NoTrust bits into KoushiSocket

--- src/com/icecondor/eaglet/api/Client.java
+++ src/com/icecondor/eaglet/api/Client.java
@@ -2,16 +2,6 @@ package com.icecondor.eaglet.api;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.apache.http.conn.ssl.SSLSocketFactory;
 
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.AsyncHttpGet;
@@ -26,19 +16,7 @@ public class Client {
         apiUrl = new URI(serverURL);
         //socket = new TooTallSocket(apiUrl, new Dispatch());
         client = AsyncHttpClient.getDefaultInstance();
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[] { new NoTrustManager() }, null);
-            client.getSSLSocketMiddleware().setSSLContext(sslContext);
-            client.getSSLSocketMiddleware().setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-            client.getSSLSocketMiddleware().setTrustManagers(new TrustManager[]{new NoTrustManager()});
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        KoushiSocket.disableSSLCheck(client);
     }
 
     public void connect() {
@@ -48,21 +26,6 @@ public class Client {
         get.setTimeout(2500);
         client.websocket(get, null, new KoushiSocket(new Dispatch()));
         //com.koushikdutta.async.http.ConnectionClosedException) {
-    }
-
-    private class NoTrustManager implements X509TrustManager {
-        @Override
-        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
-
-        @Override
-        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
-
-        @Override
-        public X509Certificate[] getAcceptedIssuers() {
-            return null;
-        }
     }
 
 }
--- src/com/icecondor/eaglet/api/KoushiSocket.java
+++ src/com/icecondor/eaglet/api/KoushiSocket.java
@@ -1,5 +1,19 @@
 package com.icecondor.eaglet.api;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+import android.util.Log;
+
+import com.icecondor.eaglet.Constants;
 import com.koushikdutta.async.ByteBufferList;
 import com.koushikdutta.async.DataEmitter;
 import com.koushikdutta.async.callback.DataCallback;
@@ -22,9 +36,11 @@ public class KoushiSocket implements AsyncHttpClient.WebSocketConnectCallback {
             return;
         }
 
+        Log.d(Constants.APP_TAG, "ws: onCompleted");
         webSocket.setStringCallback(new StringCallback() {
             @Override
             public void onStringAvailable(String s) {
+                Log.d(Constants.APP_TAG, "ws: onStringAvailable: "+s);
                 System.out.println("I got a string: " + s);
                 dispatch.process(s);
             }
@@ -33,10 +49,42 @@ public class KoushiSocket implements AsyncHttpClient.WebSocketConnectCallback {
         webSocket.setDataCallback(new DataCallback() {
             @Override
             public void onDataAvailable(DataEmitter emitter, ByteBufferList byteBufferList) {
-                System.out.println("I got some bytes!");
+                Log.d(Constants.APP_TAG, "ws: onStringAvailable ");
                 byteBufferList.recycle();
             }
         });
     }
 
+    public static void disableSSLCheck(AsyncHttpClient client) {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[] { new NoTrustManager() }, null);
+            client.getSSLSocketMiddleware().setSSLContext(sslContext);
+            client.getSSLSocketMiddleware().setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            client.getSSLSocketMiddleware().setTrustManagers(new TrustManager[]{new NoTrustManager()});
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+final class NoTrustManager implements X509TrustManager {
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
 }
