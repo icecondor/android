6598c959668966dacce11ab2fa408979ddf8bb18
Don Park <donp@donp.org>
2014-05-18 10:56:14 -0700

    listen for enter.

--- src/com/icecondor/eaglet/ui/login/LoginFragment.java
+++ src/com/icecondor/eaglet/ui/login/LoginFragment.java
@@ -3,14 +3,21 @@ package com.icecondor.eaglet.ui.login;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.icecondor.eaglet.Constants;
 import com.icecondor.eaglet.R;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements OnEditorActionListener {
+    private EditText emailField;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -23,6 +30,18 @@ public class LoginFragment extends Fragment {
         Log.d(Constants.APP_TAG, "LoginFragment onCreateView");
         View rootView = inflater.inflate(R.layout.fragment_login,
                                container, false);
+        emailField = (EditText)rootView.findViewById(R.id.login_email_field);
+        emailField.setOnEditorActionListener(this);
         return rootView;
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if(v.getId() == R.id.login_email_field) {
+            if(actionId == EditorInfo.IME_ACTION_SEND) {
+                Log.d(Constants.APP_TAG, "LoginFragment: action: "+actionId+" emailField "+v.getText());
+            }
+        }
+        return false;
     }
 }
