74616ab0c8ea0c3a2a3e905065f0af733a08c90d
Don Park <donp@donp.org>
2014-05-18 17:25:38 -0700

    db type Connected

--- src/com/icecondor/eaglet/db/Connected.java
+++ src/com/icecondor/eaglet/db/Connected.java
@@ -2,20 +2,19 @@ package com.icecondor.eaglet.db;
 
 import android.content.ContentValues;
 
-public class Connecting extends DbActivity  {
-    private static final String VERB = "connecting";
-    private final String url;
+public class Connected extends DbActivity  {
+    private static final String VERB = "connected";
 
-    public Connecting(String url) {
-        this.url = url;
+    public Connected() {
     }
 
     @Override
     public ContentValues getAttributes() {
         ContentValues cv = super.getAttributes();
         cv.put(Database.ACTIVITIES_VERB, VERB);
-        cv.put(Database.ACTIVITIES_DESCRIPTION, ""+url);
+        cv.put(Database.ACTIVITIES_DESCRIPTION, "");
         cv.put(Database.ACTIVITIES_JSON, json.toString());
         return cv;
     }
+
 }
