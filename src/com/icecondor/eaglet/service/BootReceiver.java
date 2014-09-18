c87df2f578866776bc12d0b0e01b46acb18c37be
Don Park <donp@donp.org>
2014-09-18 11:42:30 -0700

    boot receiver fixes

--- src/com/icecondor/eaglet/service/BootReceiver.java
+++ src/com/icecondor/eaglet/service/BootReceiver.java
@@ -9,25 +9,22 @@ import android.util.Log;
 
 import com.icecondor.eaglet.Condor;
 import com.icecondor.eaglet.Constants;
-import com.icecondor.eaglet.db.Database;
-import com.icecondor.eaglet.db.activity.HeartBeat;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Database db = new Database(context);
-        db.open();
+        //Database db = new Database(context);
+        //db.open();
 
         String action = intent.getAction();
         if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
             Log.i(Constants.APP_TAG, "BootReceiver onReceive "+
                      context.getClass().getSimpleName()+" now "+new Date());
-            db.append(new HeartBeat("Phone power on"));
-            ((Condor)context).binder.onNewActivity();
-            ((Condor)context).pushActivities();
+            //db.append(new HeartBeat("Phone power on"));
+            context.startService(new Intent(context, Condor.class));
         }
 
-        db.close();
+        //db.close();
     }
 
 
